e0f133fbf94362c09fa2485cd414e98a8d399ded
Jasmine Smith <jasminesmit1@unomaha.edu>
2019-11-07 18:54:29 -0600

    Revert "new changes"
    This reverts commit 2af0289497ed2c2b0ced3fac655485bd988a90bf.

--- educa/settings.py
+++ educa/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.environ.get('NAME'),
         'USER': os.environ.get('USER'),
-        'PASSWORD': os.environ.get('PASSWORD'),
+        'PASSWORD':os.environ.get('PASSWORD'),
         'HOST': os.environ.get('HOST'),
         'PORT': os.environ.get('PORT'),
     }
